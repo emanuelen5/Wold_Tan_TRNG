ecbd7ff767fa3dd296e053ce7c8ae4b9ffe6958a
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2017-06-10 18:10:05 +0200

    Fixed sim and synth support in Modelsim/Quartus
    * Simulator was not able to handle a record with mixed type
      signals (one part that is concurrent and another that is
      registered).
    * Added keep attribute to the oscillator ring to keep it from
      being removed during optimization.
    * Added some variable delay times in each oscillator ring to
      get a better approximation of real behaviour. Should change this
      to random numbers (no pun intended) in future.

--- Wold_Tan_TRNG.vhd
+++ Wold_Tan_TRNG.vhd
@@ -20,15 +20,14 @@ end entity; -- Wold_Tan_TRNG
 
 architecture rtl of Wold_Tan_TRNG is
 
-  constant C_PROP_DELAY : time := 10 ps;
+  constant C_PROP_DELAY : time := 9.837 ps;
 
-  type oscillator_ring_t is record
-    ring    : std_logic_vector(G_INVERTER_DEPTH-1 downto 0);
-    reg_out : std_logic;
-  end record;
-
-  type oscillator_ring_ary_t is array (natural range <>) of oscillator_ring_t;
+  type oscillator_ring_ary_t is array (natural range <>) of std_logic_vector(G_INVERTER_DEPTH-1 downto 0);
   signal oscillator_ring : oscillator_ring_ary_t(G_N_RINGS-1 downto 0);
+  attribute keep: boolean;
+  attribute keep of oscillator_ring: signal is true;
+
+  signal ring_reg_out    : std_logic_vector(G_N_RINGS-1 downto 0);
   signal bit_out_i, xor_sample : std_logic;
 begin
   assert (G_INVERTER_DEPTH mod 2 = 1) report "There must be an odd number of inverters in the oscillator rings to become astable." severity failure;
@@ -40,10 +39,9 @@ begin
   gen_oscillator_rings : for i in 0 to G_N_RINGS-1 generate
 
     -- Connect the oscillator rings in a loop (moving downward). The output (bottom) is also connected to the reset.
-    oscillator_ring(i).ring(0) <= not oscillator_ring(i).ring(G_INVERTER_DEPTH-1) when rst_n = '1' else '0' after C_PROP_DELAY;
-    --oscillator_ring(i).ring(0) <= '1' when rst_n = '1' else '0' after C_PROP_DELAY;
+    oscillator_ring(i)(0) <= not oscillator_ring(i)(1) when rst_n = '1' else '0' after (C_PROP_DELAY + (i * 63123 fs));
     gen_oscillator_ring : for j in 1 to G_INVERTER_DEPTH-1 generate
-      oscillator_ring(i).ring(j) <= not oscillator_ring(i).ring((j+1) mod G_INVERTER_DEPTH) after C_PROP_DELAY;
+      oscillator_ring(i)(j) <= not oscillator_ring(i)((j+1) mod G_INVERTER_DEPTH) after (C_PROP_DELAY + (i * 63521 fs) - (j * 4371 fs));
     end generate; -- gen_oscillator_ring
   end generate; -- gen_oscillator_rings
 
@@ -63,22 +61,18 @@ begin
     if (rst_n = '0') then
       bit_out_i  <= '0';
       xor_sample <= '0';
-
-      -- Reset all of the registered outputs from the oscillator rings
-      rst_regs : for i in 0 to G_N_RINGS-1 loop
-        oscillator_ring(i).reg_out <= '0';
-      end loop; -- rst_regs
+      ring_reg_out <= (others => '0');
     elsif rising_edge(clk_sys) then
 
       -- Sample the oscillator rings
       sample_oscillator_rings : for i in 0 to G_N_RINGS-1 loop
-        oscillator_ring(i).reg_out <= oscillator_ring(i).ring(0);
+        ring_reg_out(i) <= oscillator_ring(i)(0);
       end loop ; -- sample_oscillator_rings
 
       -- XOR the registered outputs of the oscillator rings
       xor_result_v := '0';
       xor_oscillator_rings : for i in 0 to G_N_RINGS-1 loop
-        xor_result_v := xor_result_v xor oscillator_ring(i).reg_out;
+        xor_result_v := xor_result_v xor ring_reg_out(i);
       end loop ; -- xor_oscillator_rings
       xor_sample <= xor_result_v;
       bit_out_i <= xor_sample; -- Double register the sampled value
